0a2309f265280ef1062cdfe9684cb8ccaf6c4546
zhangniuniu <n1997z@163.com>
2020-09-08 14:19:48 +0800

    init

--- test/web_ut.py
+++ test/web_ut.py
@@ -12,7 +12,7 @@ class ISelenium(unittest.TestCase):
     # 读入配置文件
     def get_config(self):
         config = configparser.ConfigParser()
-        config.read(os.path.join(os.environ['HOME'], 'iselenium.ini'))
+        config.read(os.path.join('../', 'iselenium.ini'))
         return config
 
     def tearDown(self):
